bffdcb73df9c189c83a3a363648793db3bb7606e
Chibundu <ndubest56@gmail.com>
2025-01-16 07:42:17 +0100

    footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,17 @@
+import CheckoutIcon from '@/app/icons/CheckoutIcon'
+import HomeIcon from '@/app/icons/HomeIcon'
 import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div className=' sticky'>
-      <Link href={'/'}>Home</Link>
-      <Link href={'/checkout'}>checkout</Link>
+    <div className=' sticky p-5 border-t border-t-gray-400 flex gap-4 items-center justify-center w-full'>
+      <Link href={'/'} className='flex gap-2'>
+        <HomeIcon /> Home
+      </Link>
+      <Link href={'/checkout'} className='flex gap-2'>
+        <CheckoutIcon /> checkout
+      </Link>
     </div>
   )
 }
